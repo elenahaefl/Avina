ea6297a35d55724071888b3aeaa2472afb97fa39
elenahaefl <elena.haefliger@bluewin.ch>
2025-05-14 15:27:30 +0200

    Profile css updates

--- js/profile.js
+++ js/profile.js
@@ -16,13 +16,11 @@ const domlastName = document.querySelector('#lastname');
 const dombirthday = document.querySelector('#userbirthday');
 const domuserage = document.querySelector('#userage');
 const domuseremail = document.querySelector('#useremail');
-const dombirthdate = document.querySelector('#inputbirthdate');
 
 domfirstName.innerHTML = data.user.firstname;
 domlastName.innerHTML = data.user.lastname;
-dombirthdate.value = data.user.birthdate;
 dombirthday.innerHTML = data.user.birthdate;
-domuseremail.innerHTML = data.user.email;3
+domuseremail.innerHTML = data.user.email;
 
 //__________________________________________________________
 // Alter berechnen
@@ -52,75 +50,75 @@ domuserage.innerHTML = age;
 //__________________________________________________________
 
 
-const inputfirstname = document.querySelector('#inputfirstname');
-const inputlastname = document.querySelector('#inputlastname');
-const savebutton = document.querySelector('#saveadditionalinfo');
+// const inputfirstname = document.querySelector('#inputfirstname');
+// const inputlastname = document.querySelector('#inputlastname');
+// const savebutton = document.querySelector('#saveadditionalinfo');
 
-savebutton.addEventListener('click', async (e) => {
-    let firstname = inputfirstname.value;
-    let lastname = inputlastname.value;
-    const url = '/api/profile/createprofile.php';
-    const data = {
-        firstname: firstname,
-        lastname: lastname
-    };
-    const dataAdded = await addData(url, data);
-    console.log(dataAdded);
+// savebutton.addEventListener('click', async (e) => {
+//     let firstname = inputfirstname.value;
+//     let lastname = inputlastname.value;
+//     const url = '/api/profile/createprofile.php';
+//     const data = {
+//         firstname: firstname,
+//         lastname: lastname
+//     };
+//     const dataAdded = await addData(url, data);
+//     console.log(dataAdded);
 
-});
+// });
 
-//I want to post the data to the API and then reload the page (Copilot eingabe) 
-async function addData(url, data) {
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-        return false
+// //I want to post the data to the API and then reload the page (Copilot eingabe) 
+// async function addData(url, data) {
+//     try {
+//         const response = await fetch(url, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         });
+//         return await response.json();
+//     } catch (error) {
+//         console.error(error);
+//         return false
 
-    }
-}
+//     }
+// }
 
 // __________________________________________________________
 // Update Vorname und Nachname und Geburtstag
 // __________________________________________________________
 
-    const inputbirthdate = document.querySelector('#inputbirthdate');
-    const savebirthdate = document.querySelector('#updatebirthday');
+    // const inputbirthdate = document.querySelector('#inputbirthdate');
+    // const savebirthdate = document.querySelector('#updatebirthday');
 
-    savebirthdate.addEventListener('click', async (e) => {
-        const updatebirthdate = inputbirthdate.value;
-        const url = '/api/profile/updateprofile.php';
-        const data = {
-            birthdate: updatebirthdate
-        };
-        const dataUpdated = await updateData(url, data);
-        console.log(dataUpdated);
-    });
+    // savebirthdate.addEventListener('click', async (e) => {
+    //     const updatebirthdate = inputbirthdate.value;
+    //     const url = '/api/profile/updateprofile.php';
+    //     const data = {
+    //         birthdate: updatebirthdate
+    //     };
+    //     const dataUpdated = await updateData(url, data);
+    //     console.log(dataUpdated);
+    // });
 
  //I want to update the data to the API and then reload the page (Copilot eingabe) 
-async function updateData(url, data) {
-    try {
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-        return false
+// async function updateData(url, data) {
+//     try {
+//         const response = await fetch(url, {
+//             method: 'PUT',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         });
+//         return await response.json();
+//     } catch (error) {
+//         console.error(error);
+//         return false
 
-    }
-}
+//     }
+// }
 
 // __________________________________________________________
 // Delete Vorname und Nachname und Geburtstag
